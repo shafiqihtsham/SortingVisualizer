7cd6f9173f58f48677ad4efb07dfa253d4db1eb0
Ihtsham Shafiq <Ihtsham Shafiq>
2024-02-08 22:17:29 +1100

    added option to change number of bars

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,32 @@
+interface SliderProps {
+  handleSpeedChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  speed: number;
+}
+
+const Slider = (props: SliderProps) => {
+  return (
+    <div className="flex sm:flex-row justify-center sm:min-w-[250px] min-w-[155px]">
+      <input
+        type="range"
+        id="slider"
+        min={0}
+        max={100}
+        onChange={props.handleSpeedChange}
+        className="absolute h-[40px] sm:min-w-64 w-[12rem] disabled:cursor-not-allowed disabled:text-gray-400"
+      />
+      <div className="relative bottom-[-8px] select-none">
+        <label
+          htmlFor="slider"
+          className="font-[Minecraftia] text-white text-xs sm:text-sm"
+        >
+          SPEED:{" "}
+          {(props.speed - 100) * -1 === 100
+            ? "ULTRA FAST"
+            : (props.speed - 100) * -1}
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
